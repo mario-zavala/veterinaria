fdd45245e11e8ed1436c6fe8fa9ba9c3809d6da7
mario-zavala <51720589+mario-zavala@users.noreply.github.com>
2021-12-17 10:40:54 -0600

    CAMBIOS HEADER

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
 
     return (
       <>
-        <h1 className="font-black text-5xl text-center mr-10 md:w-2/3 mx-auto">Seguimiento Pacientes{" "}
+        <h1 className="font-black text-5xl text-center mr-10 md:w-2/3 mx-auto">Seguimiento Pacientes Veterinaria GTO{" "}
         <span className="text-indigo-600">Veterinaria</span> </h1>
       
       </>
